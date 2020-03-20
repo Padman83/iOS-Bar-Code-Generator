5ae85ad1d1777d063482663e7793759e4b04ea27
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-03-20 13:22:02 +0800

    Initial Commit

---
+++ iOS Bar Code Generator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Bar Code Generator
+//
+//  Created by Padman on 20/03/2020.
+//  Copyright © 2020 Padman Selvamanickam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
