8f7432be0355b6eda08ce32c906fb38175f60729
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-03-25 02:44:57 +0800

    4 files added.

--- iOS Bar Code Generator/ViewController.swift
+++ iOS Bar Code Generator/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func generateBarTapped(_ sender: Any) {
+        imageView.image = generateBarCode(textField.text ?? "")
+    }
+    
+    func generateBarCode(_ string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CICode128BarcodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
